e3d1c89ca2c70c71e9b7c35001081b0934b1d07e
AmineHLub <Amino.bhl@gmail.com>
2022-05-08 01:47:27 +0100

    Add desktop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { useState } from 'react';
 import Taskbar from './components/Taskbar';
+import Desktop from './components/Desktop';
 
 function App() {
+  const [taskState, settaskState] = useState(false);
   return (
     <main>
-      <Taskbar />
+      <Desktop />
+      <Taskbar taskState={taskState} settaskState={settaskState} />
     </main>
   );
 }
---
+++ src/components/Desktop.js
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import '../stylesheets/desktop.css';
+import AppWindow from './Desktop/AppWindow';
+
+export default function Desktop() {
+  const openApps = [1, 2, 3];
+  return (
+    <div className="desktop-wrapper">
+      {
+        openApps.map((app) => (
+          <AppWindow key={app} />
+        ))
+      }
+    </div>
+  );
+}
