12338bac2ecde62dbc469ecafb5e53b4372f694c
AmineHLub <Amino.bhl@gmail.com>
2022-05-08 01:48:07 +0100

    taskbar fixing and news

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -1,16 +1,16 @@
+/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 import '../stylesheets/taskbar.css';
 import axios from 'axios';
 import TimeAndSettings from './Taskbar/TimeAndSettings';
 import TaskMenu from './Taskbar/TaskMenu';
 
-export default function Taskbar() {
-  const [taskState, settaskState] = useState(false);
+export default function Taskbar({ taskState, settaskState }) {
   const [feedResponse, setFeedResponse] = useState();
   useEffect(async () => {
     const baseUrl = 'https://newsapi.org/v2/everything?'
     + 'q=Microsoft&'
-    + 'from=2022-03-18&'
+    + 'from=2022-04-18&'
     + 'sortBy=popularity&'
     + 'apiKey=437d3b8f9c484293997c0c9027a39e23';
     const feedData = await axios.get(baseUrl);
