c35a0206d7b1e3b0eae4a9194ee30c9dfd733cc7
AmineHLub <Amino.bhl@gmail.com>
2022-05-10 01:19:11 +0100

    add shortcuts and handlers

---
+++ src/components/Desktop/ShortCuts/ShortCuts.js
@@ -0,0 +1,24 @@
+const shortCuts = [
+  {
+    name: 'Edge',
+    icon: 'https://i.ibb.co/C2zjVS5/edge-ico.png',
+    index: 1,
+  },
+  {
+    name: 'Notepad',
+    icon: 'https://i.ibb.co/th15VzL/notepad-ico.png',
+    index: 2,
+  },
+  {
+    name: 'Paint',
+    icon: 'https://i.ibb.co/Ydp4NKK/paint-ico.png',
+    index: 3,
+  },
+  {
+    name: 'Spotify',
+    icon: 'https://i.ibb.co/bPMhqx1/spotify-ico.png',
+    index: 4,
+  },
+];
+
+export default shortCuts;
