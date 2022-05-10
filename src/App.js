06790647ff9bcb7dc9e3179a393a836a96eb8170
AmineHLub <Amino.bhl@gmail.com>
2022-05-10 01:18:47 +0100

    Change feed load on page load not on Taskbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Taskbar from './components/Taskbar';
 import Desktop from './components/Desktop';
 
 function App() {
+  const [feedResponse, setFeedResponse] = useState();
+  useEffect(async () => {
+    const baseUrl = 'https://newsapi.org/v2/everything?'
+    + 'q=Microsoft&'
+    + 'from=2022-04-18&'
+    + 'sortBy=popularity&'
+    + 'apiKey=437d3b8f9c484293997c0c9027a39e23';
+    const feedData = await axios.get(baseUrl);
+    setFeedResponse(feedData.data.articles.slice(0, 6));
+  }, []);
+
   const [taskState, settaskState] = useState(false);
+  const [openedApps, setOpenedApps] = useState([]);
   return (
     <main>
-      <Desktop />
-      <Taskbar taskState={taskState} settaskState={settaskState} />
+      <Desktop settaskState={settaskState} openedApps={openedApps} setOpenedApps={setOpenedApps} />
+      <Taskbar feedResponse={feedResponse} taskState={taskState} settaskState={settaskState} />
     </main>
   );
 }
