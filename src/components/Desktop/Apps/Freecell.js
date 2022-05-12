abc07682032e469cbe9e9f958cf6338fb0cca761
AmineHLub <Amino.bhl@gmail.com>
2022-05-12 01:15:14 +0100

    Add freecell

---
+++ src/components/Desktop/Apps/Freecell.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Freecell() {
+  return (
+    <div className="frameapp-container">
+      <iframe className="frame-sap" title="website" loading="lazy" src="https://solitaires-online.com/freecell">
+        <p>Your browser does not support iframes.</p>
+      </iframe>
+    </div>
+  );
+}
